e2ef36761735052d9c03422dd5d5e1d16595a4b3
omeralitosun <omerttosu@gmail.com>
2023-05-27 15:32:38 +0300

    Update ınvoice controller

--- invoice-service/src/main/java/com/kodlamaio/invoiceservice/controller/InvoiceController.java
+++ invoice-service/src/main/java/com/kodlamaio/invoiceservice/controller/InvoiceController.java
@@ -1,20 +1,32 @@
 package com.kodlamaio.invoiceservice.controller;
 
-import com.kodlamaio.invoiceservice.entities.Invoice;
-import com.kodlamaio.invoiceservice.repository.InvoiceRepository;
+import com.kodlamaio.invoiceservice.business.abstracts.InvoiceService;
+import com.kodlamaio.invoiceservice.business.dto.responses.get.GetAllInvoicesResponse;
+import com.kodlamaio.invoiceservice.business.dto.responses.get.GetInvoiceResponse;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("/api/invoice")
+@RequestMapping("/api/invoices")
 public class InvoiceController {
 
-    public final InvoiceRepository repository;
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public Invoice add(@RequestBody Invoice request) {
-        return repository.save(request);
+    public final InvoiceService service;
+
+    @GetMapping
+    public List<GetAllInvoicesResponse> getAll() {
+        return service.getAll();
     }
+
+    @GetMapping("/{id}")
+    public GetInvoiceResponse getById(@PathVariable UUID id) {
+        return service.getById(id);
+    }
+
 }
